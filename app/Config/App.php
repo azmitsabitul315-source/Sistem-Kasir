0de9949e40dc4b4aa89c52f00dd6ed6ea54ccf90
azmitsabitul315-source <azmitsabitul315@gmail.com>
2026-09-10 02:34:43 +0700

    Enable dynamic baseURL detection for multi-device access

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,23 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://localhost/POSayambakar/public/';
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $protocol  = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https' : 'http';
+            $scriptDir = dirname($_SERVER['SCRIPT_NAME'] ?? '');
+            $scriptDir = rtrim(str_replace('\\', '/', $scriptDir), '/');
+            if ($scriptDir !== '') {
+                $this->baseURL = $protocol . '://' . $_SERVER['HTTP_HOST'] . $scriptDir . '/';
+            } else {
+                $this->baseURL = $protocol . '://' . $_SERVER['HTTP_HOST'] . '/';
+            }
+        }
+    }
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
